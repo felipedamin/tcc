cc2c81cdf8b97295dc6247584eccb1bb08039981
Felipe Damin <felipe.damin@bee2pay.com>
2021-06-15 16:31:38 -0300

    adding ternary expression test

--- teste/src/main/java/teste/Methods.java
+++ teste/src/main/java/teste/Methods.java
@@ -12,19 +12,20 @@ public class Methods {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        if (a < 10) {
+        if (a < 10 && true) {
             System.out.println("a é menor que 10");
         }
-        if (b > 10) {
+        if (b > 10 || b == 10) {
             System.out.println("b é maior que 10");
         }
-        if (boolean1) {
+        if (boolean1 & true) {
             System.out.println("boolean1 é true");
         }
-        if (!boolean2) {
+        if (!boolean2 | boolean2) {
             System.out.println("boolean2 é false");
         }
         System.out.println("!boolean2 é: " + !boolean2);
+        String ternary = boolean1 ? "ternario" : "falso";
     }
 
     public static void method1() {
@@ -32,6 +33,9 @@ public class Methods {
     }
 
     public static Boolean method2(boolean var) {
+        if (var) {
+            return !var;
+        }
         return var;
     }
 }
