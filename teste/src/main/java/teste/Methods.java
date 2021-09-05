bb8384c710b9672981e5546ce777b4c441872936
Felipe Damin <felipe.damin@bee2pay.com>
2021-09-04 21:26:57 -0300

    melhorias

--- teste/src/main/java/teste/Methods.java
+++ teste/src/main/java/teste/Methods.java
@@ -29,23 +29,25 @@ public class Methods {
             }
         }
     }
+
     public static void funcaoIfElseIf() {
         System.out.println("Hello World!");
-        if (a < 10 && true) {
+        if (a < 10) {
             System.out.println("a é menor que 10");
-        }
-        else if (b > 10 || b == 10) {
-            System.out.println("b é maior que 10");
-        }
-        else if ((boolean1 & true) || (true && a==b)) {
+        } else if (b > 10 || b == 10) {
             System.out.println("boolean1 é true");
-        }
-        else {
+        } else if (b != a){
+            System.out.println("boolean1 é true");
+        } else {
             System.out.println("else sem if");
         }
-        
     }
 
+    public static void funcaoIfmultiplasCondicoes() {
+        if ((boolean1 & true) || (true && a == b)){
+            System.out.println("funcaoIfmultiplasCondicoes");
+        }
+    }
     public static void funcaoTernarioNested() {
         if (!boolean2 | boolean2) {
             if (boolean1) {
@@ -55,17 +57,15 @@ public class Methods {
             String ternary = boolean1 ? "ternario" : "falso";
         }
     }
+
     public static void funcaoForWhile() {
         System.out.println("metodo 1 foi chamado");
-
         for (int i = 0; i < 3; i++) {
             System.out.println(i);
         }
-
         for (int i = 0, j = 0; i < 3 && j == i; i++, j++) {
             System.out.println(i);
         }
-        
         int whileInt = 0;
         while (whileInt <= 2) {
             System.out.println(whileInt);
@@ -77,14 +77,13 @@ public class Methods {
         if (var) {
             return !var;
         }
-        switch (switchInt + 5) {
-            case 6:    
+        switch(switchInt + 5) {
+            case 6:
                 break;
-            case 7:    
+            case 7:
                 break;
-            case 5:    
+            case 5:
                 break;
-            
             default:
                 break;
         }
