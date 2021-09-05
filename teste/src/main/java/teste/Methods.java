51d52cd240daa93162def3bde6d28a9b01a5d0fa
Felipe Damin <felipe.damin@bee2pay.com>
2021-09-04 21:22:15 -0300

    refact funcoes

--- teste/src/main/java/teste/Methods.java
+++ teste/src/main/java/teste/Methods.java
@@ -10,17 +10,25 @@ public class Methods {
 
     private static Boolean boolean2 = false;
 
-    public static void funcaoComIfElse() {
-        System.out.println("method name: funcaoComIfElse, if params: a < 1");
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        if (a < 10 && true) {
+            System.out.println("a é menor que 10");
+    }
+
+    public static void funcaoIfElseNested() {
         if (a < 1) {
             System.out.println(a);
         } else {
             if (b > 1) {
                 System.out.println(b);
             }
+            if (b == 1) {
+                System.out.println(b);
+            }
         }
     }
-    public static void main(String[] args) {
+    public static void funcaoIfElseIf() {
         System.out.println("Hello World!");
         if (a < 10 && true) {
             System.out.println("a é menor que 10");
@@ -28,21 +36,25 @@ public class Methods {
         else if (b > 10 || b == 10) {
             System.out.println("b é maior que 10");
         }
-
-        if ((boolean1 & true) || (true && a==b)) {
+        else if ((boolean1 & true) || (true && a==b)) {
             System.out.println("boolean1 é true");
         }
         else {
             System.out.println("else sem if");
         }
-        if (!boolean2 | boolean2) {
-            System.out.println("boolean2 é false");
-        }
-        System.out.println("!boolean2 é: " + !boolean2);
-        String ternary = boolean1 ? "ternario" : "falso";
+        
     }
 
-    public static void method1() {
+    public static void funcaoTernarioNested() {
+        if (!boolean2 | boolean2) {
+            if (boolean1) {
+                System.out.println(boolean1);
+            }
+            System.out.println("!boolean2 é: " + !boolean2);
+            String ternary = boolean1 ? "ternario" : "falso";
+        }
+    }
+    public static void funcaoForWhile() {
         System.out.println("metodo 1 foi chamado");
 
         for (int i = 0; i < 3; i++) {
@@ -60,7 +72,7 @@ public class Methods {
         }
     }
 
-    public static Boolean method2(boolean var, int switchInt) {
+    public static Boolean funcaoSwitch(boolean var, int switchInt) {
         if (var) {
             return !var;
         }
