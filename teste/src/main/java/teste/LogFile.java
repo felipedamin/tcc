10457f16b68153526e9e3ca01b5e712a5417c49c
Felipe A S Damin <felipes.damin96@gmail.com>
2021-10-29 18:41:26 -0300

    updating LogFile identity

--- teste/src/main/java/teste/LogFile.java
+++ teste/src/main/java/teste/LogFile.java
@@ -1,18 +1,20 @@
 package teste;
 
-import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
 public class LogFile {
 
-    public static void write(String className, String methodName, Object object) throws IOException {
+    public static void write(String className, String methodName, String conditionType, String[] conditions, Object[] paramValue, boolean[] values) throws IOException {
         FileWriter file = new FileWriter("logFile.out", true);
-        String t = object.toString();
-        System.out.println(t);
         // array, se for nested, se nao for, loga mais uma linha
         System.out.println("class#methodName#[statementType1:params1,...,statementTypeN:paramsN]");
-        String formatted = String.format("%s#%s#%s\n", className, methodName, t);
+
+        // ordem de implementaçao:
+        // "class#methodName#[a<10,b!=a]#[true,false]
+        // "class#methodName#[a<10,b!=a]#[a.toString(),b.toString()]#[true,false]
+        // "class#methodName#(if|for|while|switch)#[a<10,b!=a]#[a,b]#[a.toString(),b.toString()]#[true,false]"
+        String formatted = String.format("%s#%s#%s\n", className, methodName);
         try {
             file.write(formatted);
         } catch (IOException e) {
