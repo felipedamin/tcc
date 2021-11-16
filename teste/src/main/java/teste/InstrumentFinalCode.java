ae72f112c6cc78d4b0107b59affdff7b4a61f62a
VictorMSKim <victor.min.kim@usp.br>
2021-11-16 20:29:07 -0300

    instrument final code, change hashmap to use arraylist

--- teste/src/main/java/teste/InstrumentFinalCode.java
+++ teste/src/main/java/teste/InstrumentFinalCode.java
@@ -1,6 +1,7 @@
 package teste;
 
 import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.Map;
 import java.util.Scanner;
 import java.io.File;
@@ -14,19 +15,28 @@ public class InstrumentFinalCode {
     }
 
     public static Map instrument() throws FileNotFoundException {
-        Map outerMap = new HashMap();
+        Map<String, ArrayList<String>> outerMap = new HashMap();
         File file = new File("/Users/victorkim/Documents/POLI/TCC/tcc/logFile.out");
         Scanner sc = new Scanner(file);
-        int counter = 0;
+        // int counter = 0;
         while (sc.hasNextLine()) {
             String line = sc.nextLine();
             String[] split = line.split("\\#");
-            Map map = new HashMap();
-            map.put("className", split[0]);
-            map.put("methodName", split[1]);
-            map.put("condition", split[2]);
-            outerMap.put(counter, map);
-            counter++;
+            // Array map = new HashMap();
+            String className = split[0];
+            String methodName = split[1];
+            String condition = split[2];
+            if (!outerMap.containsKey(className + "#" + methodName)) {
+                ArrayList<String> conditions = new ArrayList<String>();
+                conditions.add(condition);
+                outerMap.put(className + "#" + methodName, conditions);
+            }
+            else {
+                ArrayList<String> conditions = outerMap.get(className + "#" + methodName);
+                if (!conditions.contains(condition)) {
+                    conditions.add(condition);
+                }
+            }
         }
         return outerMap;
     }
