138f1d3d3b0dc92585e70b2856489c5c9e3f8707
VictorMSKim <victor.min.kim@usp.br>
2021-11-23 21:58:00 -0300

    add mvn install

--- teste/src/main/java/teste/install.sh
+++ teste/src/main/java/teste/install.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 #
 
-apt-get update
+# install python and pandas
+apt install python3 -y
+apt install python3-pip -y
+pip3 install pandas
+
+# get to root directory and install javaparser and jqf-fuzzer
+cd ..  
+cd ..
+cd ..
+cd ..
+mvn install
